3d82d0b9a3e70c17154b52f9eb10ecff5e681eca
Ben Andrew <bena@Chalmers.kainos.com>
2019-07-29 15:40:57 +0100

    model view page

--- app/createapplication/views.py
+++ app/createapplication/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
+from .models import Application
 
-# Create your views here.
+def index(request):
+    return render(request, 'createapplication/index.html', { "applications" : Application.objects.order_by('date') })
