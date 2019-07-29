8f1b19f6d07758782aa417347ee5b69f163c9ea2
Mark Spiers <marksp@dolphin.kainos.com>
2019-07-29 16:33:22 +0100

    added good model, and admin view

--- app/createapplication/models.py
+++ app/createapplication/models.py
@@ -1,8 +1,14 @@
 from django.db import models
 
+class Good(models.Model):
+    name = models.CharField(max_length=200)
+    def __str__(self):
+        return self.name
+
 class Application(models.Model):
     name = models.CharField(max_length=200)
     date = models.DateTimeField()
     destination = models.CharField(max_length=200)
+    goods = models.ManyToManyField(Good)
     def __str__(self):
         return self.name
