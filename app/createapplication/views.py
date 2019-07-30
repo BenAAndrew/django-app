3e5728bacf4cdfc02fb82f8af7620efbce84581b
Ben Andrew <bena@Chalmers.local>
2019-07-30 11:50:52 +0100

    abc

--- app/createapplication/views.py
+++ app/createapplication/views.py
@@ -1,11 +1,8 @@
-from django.http import HttpResponse, JsonResponse
+from django.http import HttpResponse
 from django.shortcuts import render
 from rest_framework.parsers import JSONParser
-from rest_framework.response import Response
 
 from .models import Application
-from datetime import datetime
-from .serializers import ApplicationSerializer
 
 def index(request):
     return render(request, 'createapplication/index.html', { "applications" : Application.objects.order_by('date') })
@@ -14,5 +11,10 @@ def detail(request, application_id):
     return HttpResponse("You're looking at question %s." % application_id)
 
 def create(request):
-    return render(request, 'createapplication/create.html')
+    if request.method == "GET":
+        return render(request, 'createapplication/create.html')
+    elif request.method == "POST":
+        parsed = JSONParser().parse(request)
+        request.post("http://127.0.0.1:8001/application", data=parsed)
+        return HttpResponse("SENT")
 
