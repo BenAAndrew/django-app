3c7ac1c3fc323e26afd54e8fe7d495fcebb613be
Ben Andrew <bena@Chalmers.kainos.com>
2019-07-30 14:30:18 +0100

    get applications

--- app/createapplication/views.py
+++ app/createapplication/views.py
@@ -2,10 +2,18 @@ import requests
 from django.http import HttpResponse, JsonResponse
 from django.shortcuts import render
 from django.views.decorators.csrf import csrf_exempt
+from rest_framework.parsers import JSONParser
+import json
+
+from .serializers import ApplicationSerializer
 from .models import Application
 
+def getApplications():
+    r = requests.get('http://127.0.0.1:8001/application/')
+    return json.loads(r.content.decode('utf-8'))
+
 def index(request):
-    return render(request, 'createapplication/index.html', { "applications" : Application.objects.order_by('date') })
+    return render(request, 'createapplication/index.html', { "applications" : getApplications() })
 
 def detail(request, application_id):
     return HttpResponse("You're looking at question %s." % application_id)
@@ -16,4 +24,4 @@ def create(request):
         return render(request, 'createapplication/create.html')
     elif request.method == "POST":
         r = requests.post('http://127.0.0.1:8001/application/', data=request.body)
-        return HttpResponse(r)
+        return render(request, 'createapplication/applicationRedirect.html')
