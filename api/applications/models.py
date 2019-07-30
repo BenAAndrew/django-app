4637a4427d487bea10419f2d104742d958017eda
Mark Spiers <marksp@dolphin.kainos.com>
2019-07-30 11:06:59 +0100

    started moving stuff to api directory

--- api/applications/models.py
+++ api/applications/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.utils.timezone import now
 
-# Create your models here.
+class Good(models.Model):
+    name = models.CharField(max_length=200)
+    def __str__(self):
+        return self.name
+
+class Application(models.Model):
+    name = models.CharField(max_length=200)
+    date = models.DateTimeField(default=now)
+    destination = models.CharField(max_length=200)
+    goods = models.ManyToManyField(Good)
+    def __str__(self):
+        return self.name
--- app/createapplication/views.py
+++ app/createapplication/views.py
@@ -16,26 +16,3 @@ def detail(request, application_id):
 def create(request):
     return render(request, 'createapplication/create.html')
 
-def add(request):
-    app = Application(name=request.POST['name'], date=datetime.now(), destination=request.POST['destination'])
-    app.save()
-    return HttpResponse(app)
-
-def application(request, application_id):
-    try:
-        application = Application.objects(pk=application_id)
-    except Application.DoesNotExit:
-        return HttpResponse(status=404)
-
-def application(request):
-    if request.method == 'GET':
-        applications = Application.objects.all()
-        serializer = ApplicationSerializer(applications, many=True)
-        return JsonResponse(serializer.data, safe=False, status=200)
-    if request.method == 'POST':
-        data = JSONParser().parse(request)
-        serializer = ApplicationSerializer(data=data)
-        if serializer.is_valid():
-            serializer.save()
-            return JsonResponse(serializer.data, status=201)
-        return JsonResponse(serializer.errors, status=400)
