b9e0e1822b82f3a7e26bf3ad3f51eb43170491f0
Ben Andrew <bena@Chalmers.kainos.com>
2019-07-31 17:28:50 +0100

    navbar and add good test

--- pyTest/applications/test_Application.py
+++ pyTest/applications/test_Application.py
@@ -1,6 +1,7 @@
 import pytest
 import pytest_html
 from selenium import webdriver
+from time import sleep
 
 #Fixture for Chrome
 @pytest.fixture(scope="class")
@@ -14,6 +15,24 @@ def chrome_driver_init(request):
 class Chrome:
     pass
 
-class TestClass(Chrome):
-    def test_one(self):
-        self.driver.get("https://www.amazon.co.uk/")
+url = "http://127.0.0.1:8000/"
+id_to_link = {"home" : "application/", "createApplication" : "application/create/",
+              "createGood" : "application/creategood/", "viewGood" : "application/goods/"}
+testGood = "testGood"
+
+class TestNavbar(Chrome):
+    def test_each_link(self):
+        self.driver.get(url+"application/")
+        for id in id_to_link:
+            self.driver.find_element_by_id(id).click()
+            assert self.driver.current_url == url+id_to_link[id]
+
+class TestAddingData(Chrome):
+    def test_add_good(self):
+        self.driver.get(url+"application/creategood/")
+        self.driver.find_element_by_name("name").send_keys(testGood)
+        self.driver.find_element_by_xpath("//input[@type=\"submit\"]").click()
+        self.driver.switch_to.alert.accept()
+        self.driver.get(url + "application/goods/")
+        goodNames = self.driver.find_elements_by_xpath("//div//h1")
+        assert testGood in [name.get_attribute('innerHTML') for name in goodNames]
