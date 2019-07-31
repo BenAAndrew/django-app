d5cff8be6e46e1114163dceeb80bdfbd4fedccf9
Ben Andrew <bena@Chalmers.kainos.com>
2019-07-31 17:29:56 +0100

    url corrections

--- app/createapplication/urls.py
+++ app/createapplication/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('<int:application_id>/', views.detail, name='detail'),
-    path('create', views.createApplication, name='create'),
+    path('create/', views.createApplication, name='create'),
     path('editapplication/<int:application_id>/', views.editApplication, name='editApplication'),
     path('viewapplication/<int:application_id>/', views.viewApplication, name='viewApplication'),
     path('deleteapplication/<int:application_id>/', views.deleteApplication, name='deleteApplication'),
@@ -12,5 +12,5 @@ urlpatterns = [
     path('editgood/<int:good_id>/', views.editGood, name='editGood'),
     path('viewgood/<int:good_id>/', views.viewGood, name='viewGood'),
     path('deletegood/<int:good_id>/', views.deleteGood, name='deleteGood'),
-    path('creategood', views.createGood, name='good')
+    path('creategood/', views.createGood, name='good')
 ]
