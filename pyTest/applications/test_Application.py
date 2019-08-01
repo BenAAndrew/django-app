0b6ebdc4a6e6ac378ca5fe4126faf58858aaa0e0
Ben Andrew <bena@Chalmers.local>
2019-08-01 09:58:38 +0100

    form interation

--- pyTest/applications/test_Application.py
+++ pyTest/applications/test_Application.py
@@ -4,6 +4,9 @@ from selenium import webdriver
 from time import sleep
 
 #Fixture for Chrome
+from selenium.webdriver.support.select import Select
+
+
 @pytest.fixture(scope="class")
 def chrome_driver_init(request):
     chrome_driver = webdriver.Chrome()
@@ -15,10 +18,13 @@ def chrome_driver_init(request):
 class Chrome:
     pass
 
+
 url = "http://127.0.0.1:8000/"
 id_to_link = {"home" : "application/", "createApplication" : "application/create/",
               "createGood" : "application/creategood/", "viewGood" : "application/goods/"}
 testGood = "testGood"
+testApp = {"name" : "testApp", "destination" : "testLocation"}
+
 
 class TestNavbar(Chrome):
     def test_each_link(self):
@@ -27,12 +33,23 @@ class TestNavbar(Chrome):
             self.driver.find_element_by_id(id).click()
             assert self.driver.current_url == url+id_to_link[id]
 
+
 class TestAddingData(Chrome):
     def test_add_good(self):
-        self.driver.get(url+"application/creategood/")
+        self.driver.get(url+ id_to_link["createGood"])
         self.driver.find_element_by_name("name").send_keys(testGood)
         self.driver.find_element_by_xpath("//input[@type=\"submit\"]").click()
         self.driver.switch_to.alert.accept()
-        self.driver.get(url + "application/goods/")
+        self.driver.get(url + id_to_link["viewGood"])
         goodNames = self.driver.find_elements_by_xpath("//div//h1")
         assert testGood in [name.get_attribute('innerHTML') for name in goodNames]
+
+    def test_add_application(self):
+        self.driver.get(url + id_to_link["createApplication"])
+        self.driver.find_element_by_name("name").send_keys(testApp["name"])
+        self.driver.find_element_by_name("destination").send_keys(testApp["destination"])
+        select = Select(self.driver.find_element_by_name("goods"))
+        select.select_by_visible_text(testGood)
+        self.driver.find_element_by_xpath("//input[@type=\"submit\"]").click()
+        self.driver.get(url + id_to_link["home"])
+        sleep(10)
