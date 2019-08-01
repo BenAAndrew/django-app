552e71ce8251df63de1a86e3b7857d2d3b2a4a29
Ben Andrew <bena@Chalmers.local>
2019-08-01 11:41:19 +0100

    removed x_paths

--- pyTest/applications/test_Application.py
+++ pyTest/applications/test_Application.py
@@ -38,10 +38,10 @@ class TestAddingData(Chrome):
     def test_add_good(self):
         self.driver.get(url+ id_to_link["createGood"])
         self.driver.find_element_by_name("name").send_keys(testGood)
-        self.driver.find_element_by_xpath("//input[@type=\"submit\"]").click()
+        self.driver.find_element_by_id("submit").click()
         self.driver.switch_to.alert.accept()
         self.driver.get(url + id_to_link["viewGood"])
-        goodNames = self.driver.find_elements_by_xpath("//div//h1")
+        goodNames = self.driver.find_elements_by_id("good_name")
         assert testGood in [name.text for name in goodNames]
 
     def test_add_application(self):
@@ -50,29 +50,30 @@ class TestAddingData(Chrome):
         self.driver.find_element_by_name("destination").send_keys(testApp["destination"])
         select = Select(self.driver.find_element_by_name("goods"))
         select.select_by_visible_text(testGood)
-        self.driver.find_element_by_xpath("//input[@type=\"submit\"]").click()
+        self.driver.find_element_by_id("submit").click()
         self.driver.switch_to.alert.accept()
-        appNames = self.driver.find_elements_by_xpath("//div//h1")
+        appNames = self.driver.find_elements_by_id("app_name")
         assert testApp["name"] in [name.text for name in appNames]
 
 
 class TestDeletingData(Chrome):
     def test_delete_good(self):
         self.driver.get(url + id_to_link["viewGood"])
-        totalGoods = len(self.driver.find_elements_by_xpath("//div//h1"))
-        self.driver.find_element_by_xpath("//button[text()=\"Edit\"]").click()
-        self.driver.find_element_by_xpath("//input[@value=\"Delete\"]").click()
+        totalGoods = len(self.driver.find_elements_by_id("good_name"))
+        self.driver.find_element_by_id("edit").click()
+        self.driver.find_element_by_id("delete").click()
         self.driver.switch_to.alert.accept()
         self.driver.get(url + id_to_link["viewGood"])
-        assert len(self.driver.find_elements_by_xpath("//div//h1")) == totalGoods - 1
+        assert len(self.driver.find_elements_by_id("good_name")) == totalGoods - 1
 
     def test_delete_application(self):
         self.driver.get(url + id_to_link["home"])
-        totalApplications = len(self.driver.find_elements_by_xpath("//div//h1"))
-        self.driver.find_element_by_xpath("//button[text()=\"Edit\"]").click()
-        self.driver.find_element_by_xpath("//input[@value=\"Delete\"]").click()
+        totalApplications = len(self.driver.find_elements_by_id("app_name"))
+        self.driver.find_element_by_id("edit").click()
+        self.driver.find_element_by_id("delete").click()
         self.driver.switch_to.alert.accept()
-        assert len(self.driver.find_elements_by_xpath("//div//h1")) == totalApplications - 1
+        assert len(self.driver.find_elements_by_id("app_name")) == totalApplications - 1
+
 
 class TestEditingData(Chrome):
     def test_edit_application(self):
@@ -103,17 +104,18 @@ class TestEditingData(Chrome):
 class TestViewingData(Chrome):
     def test_view_good(self):
         self.driver.get(url + id_to_link["viewGood"])
-        goodName = self.driver.find_element_by_xpath("//div//h1").text
-        self.driver.find_element_by_xpath("//button[text()=\"View\"]").click()
-        assert self.driver.find_element_by_xpath("//h1").text == goodName
-        assert self.driver.find_element_by_xpath("//input").get_attribute('value') == "Delete"
+        goodName = self.driver.find_element_by_id("good_name").text
+        self.driver.find_element_by_id("view").click()
+        assert self.driver.find_element_by_id("good_name").text == goodName
+        assert self.driver.find_element_by_id("delete").get_attribute('value') == "Delete"
 
     def test_view_application(self):
         self.driver.get(url + id_to_link["home"])
-        appName = self.driver.find_element_by_xpath("//div//h1").text
-        self.driver.find_element_by_xpath("//button[text()=\"View\"]").click()
-        assert self.driver.find_element_by_xpath("//h1").text == appName
-        assert self.driver.find_element_by_xpath("//input").get_attribute('value') == "Delete"
-        assert self.driver.find_element_by_xpath("//h2").text == "Goods;"
-        self.driver.find_element_by_xpath("//ul")
-        assert len(self.driver.find_elements_by_xpath("//h4")) == 2
+        appName = self.driver.find_element_by_id("app_name").text
+        self.driver.find_element_by_id("view").click()
+        assert self.driver.find_element_by_id("name").text == appName
+        assert self.driver.find_element_by_id("delete").get_attribute('value') == "Delete"
+        assert self.driver.find_element_by_id("goods").text == "Goods;"
+        assert self.driver.find_element_by_id("goods_list")
+        assert self.driver.find_elements_by_id("date")
+        assert self.driver.find_elements_by_id("destination")
