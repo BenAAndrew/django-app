2f84eabd737e48656b62155519fbffd5f7ab760c
BenAAndrew <benandrew89@gmail.com>
2019-08-07 16:04:27 +0100

    goods

--- api/applications/views.py
+++ api/applications/views.py
@@ -25,39 +25,39 @@ class ApplicationsView(GenericAPIView):
         return JsonResponse(serializer.errors, status=400)
 
 
-def process_application(application_id, inner_func):
-    try:
-        application = Application.objects.get(pk=application_id)
-        return inner_func(application)
-    except Application.DoesNotExist:
-        return HttpResponse(status=404)
-
 class ApplicationView(GenericAPIView):
     serializer_class = ApplicationSerializer
     model = Application
     queryset = Application.objects.all()
 
     def get(self, request, application_id):
-        def get_application(application):
+        try:
+            application = Application.objects.get(pk=application_id)
             serializer = ApplicationSerializer(application, many=False)
             return JsonResponse(serializer.data, safe=False)
-        return process_application(application_id, get_application)
+        except Application.DoesNotExist:
+            return HttpResponse(status=404)
 
     def put(self, request, application_id):
-        def put_application(application):
+        try:
+            application = Application.objects.get(pk=application_id)
             data = json.loads(request.body)
             serializer = ApplicationSerializer(application, data=data)
             if serializer.is_valid():
                 serializer.save()
                 return JsonResponse(serializer.data)
             return JsonResponse(serializer.errors, status=400)
-        return process_application(application_id, put_application)
+        except Application.DoesNotExist:
+            return HttpResponse(status=404)
 
     def delete(self, request, application_id):
-        def delete_application(application):
+        try:
+            application = Application.objects.get(pk=application_id)
             application.delete()
             return HttpResponse(status=204)
-        return process_application(application_id, delete_application)
+        except Application.DoesNotExist:
+            return HttpResponse(status=404)
+
 
 class ApplicationProgressView(GenericAPIView):
     serializer_class = ApplicationSerializer
@@ -73,12 +73,13 @@ class ApplicationProgressView(GenericAPIView):
             return False
 
     def get(self, request, application_id, new_progress):
-        def put_application(application):
-            print(str(application_id))
+        try:
+            application = Application.objects.get(pk=application_id)
             print(application.progress + " => " + new_progress)
             if self.check_progress_update(new_progress, application.progress):
                 application.progress = new_progress
                 application.save()
                 return HttpResponse(status=204)
             return HttpResponse(status=404)
-        return process_application(application_id, put_application)
+        except Application.DoesNotExist:
+            return HttpResponse(status=404)
