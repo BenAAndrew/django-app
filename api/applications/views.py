2578151027a5abc59a04aa33387683e0de521584
BenAAndrew <benandrew89@gmail.com>
2019-08-07 14:17:20 +0100

    application views

--- api/applications/views.py
+++ api/applications/views.py
@@ -1,34 +1,20 @@
 import json
 from django.http import HttpResponse, JsonResponse
 from rest_framework.parsers import JSONParser
+from rest_framework.generics import GenericAPIView
 from .models import Application, Good
 from .serializers import ApplicationSerializer
+from django.shortcuts import get_object_or_404
 
-def application_detail(request, application_id):
-    try:
-        application = Application.objects.get(pk=application_id)
-    except:
-        return HttpResponse(status=404)
-    if request.method == 'GET':
-        serializer = ApplicationSerializer(application, many=False)
-        return JsonResponse(serializer.data, safe=False)
-    elif request.method == 'PUT':
-        data = json.loads(request.body)
-        serializer = ApplicationSerializer(application, data=data)
-        if serializer.is_valid():
-            serializer.save()
-            return JsonResponse(serializer.data)
-        return JsonResponse(serializer.errors, status=400)
-    elif request.method == 'DELETE':
-        application.delete()
-        return HttpResponse(status=204)
+class ApplicationsView(GenericAPIView):
+    serializer_class = ApplicationSerializer
 
-def application_list(request):
-    if request.method == 'GET':
+    def get(self, request):
         applications = Application.objects.all()
         serializer = ApplicationSerializer(applications, many=True)
         return JsonResponse(serializer.data, safe=False, status=200)
-    elif request.method == 'POST':
+
+    def post(self, request):
         values = json.loads(request.body)
         serializer = ApplicationSerializer(data=values)
         if serializer.is_valid():
@@ -36,6 +22,37 @@ def application_list(request):
             return JsonResponse(serializer.data, status=201)
         return JsonResponse(serializer.errors, status=400)
 
+class ApplicationView(GenericAPIView):
+    serializer_class = ApplicationSerializer
+
+    def process_application(self, application_id, inner_func):
+        try:
+            application = Application.objects.get(pk=application_id)
+            return inner_func(application)
+        except Application.DoesNotExist:
+            return HttpResponse(status=404)
+
+    def get(self, request, application_id):
+        def get_application(application):
+            serializer = ApplicationSerializer(application, many=False)
+            return JsonResponse(serializer.data, safe=False)
+        return self.process_application(application_id, get_application)
+
+    def put(self, request, application_id):
+        def put_application(application):
+            data = json.loads(request.body)
+            serializer = ApplicationSerializer(application, data=data)
+            if serializer.is_valid():
+                serializer.save()
+                return JsonResponse(serializer.data)
+            return JsonResponse(serializer.errors, status=400)
+        return self.process_application(application_id, put_application)
+
+    def delete(self, request, application_id):
+        def delete_application(application):
+            application.delete()
+            return HttpResponse(status=204)
+        return self.process_application(application_id, delete_application)
 
 def process_application(application_id, current_state, new_state):
     try:
