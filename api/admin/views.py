6a9b7ccab84257df84b379f158122f998e01c160
BenAAndrew <benandrew89@gmail.com>
2019-08-08 16:30:25 +0100

    admin api check

--- api/admin/views.py
+++ api/admin/views.py
@@ -1,5 +1,5 @@
 from django.http import JsonResponse, HttpResponse
-from rest_framework.generics import GenericAPIView
+from rest_framework.generics import ListAPIView, RetrieveAPIView
 
 from applications.models import Application
 from applications.serializers import ApplicationSerializer
@@ -9,7 +9,8 @@ from users.views import TokenHandler
 
 tokenHandler = TokenHandler()
 
-def isAdmin(request):
+def isAdmin(*args):
+    request = args[1]
     try:
         token = tokenHandler.decode_token(request.COOKIES["token"])
         return token["admin"]
@@ -25,21 +26,23 @@ def check_admin(input_func):
             return HttpResponse(status=401)
     return admin
 
-class AdminApplicationsView(GenericAPIView):
+class AdminApplicationsView(ListAPIView):
     serializer_class = ApplicationSerializer
     model = Application
     queryset = Application.objects.all()
 
+    @check_admin
     def get(self, request):
         applications = Application.objects.all()
         serializer = ApplicationSerializer(applications, many=True)
         return JsonResponse(serializer.data, safe=False, status=200)
 
-class AdminApplicationView(GenericAPIView):
+class AdminApplicationView(RetrieveAPIView):
     serializer_class = ApplicationSerializer
     model = Application
     queryset = Application.objects.all()
 
+    @check_admin
     def get(self, request, application_id):
         try:
             application = Application.objects.get(pk=application_id)
@@ -48,21 +51,23 @@ class AdminApplicationView(GenericAPIView):
         except Application.DoesNotExist:
             return HttpResponse(status=404)
 
-class AdminGoodsView(GenericAPIView):
+class AdminGoodsView(ListAPIView):
     serializer_class = GoodSerializer
     model = Good
     queryset = Good.objects.all()
 
+    @check_admin
     def get(self, request):
         goods = Good.objects.all()
         serializer = GoodSerializer(goods, many=True)
         return JsonResponse(serializer.data, safe=False)
 
-class AdminGoodView(GenericAPIView):
+class AdminGoodView(RetrieveAPIView):
     serializer_class = GoodSerializer
     model = Good
     queryset = Good.objects.all()
 
+    @check_admin
     def get(self, request, good_id):
         try:
             good = Good.objects.get(pk=good_id)
